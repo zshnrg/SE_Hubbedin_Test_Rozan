2b4cd759eff89c324a6b36030d376d0a6a532aa2
zshnrg <ghosanirozan1@gmail.com>
2025-03-30 03:25:20 +0700

    feat: add mocked mail service

---
+++ services/mailer.service.js
@@ -0,0 +1,5 @@
+export const sendMail = async (to, subject, recipient) => {
+  // Simulate sending an email
+  console.log(`Sending email to ${to} with subject "${subject}" and recipient "${recipient}"`);
+  return Promise.resolve();
+}
