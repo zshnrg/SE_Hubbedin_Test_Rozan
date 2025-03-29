07b6379a2cf2d029c2cef19de7f0fdef8f385955
zshnrg <ghosanirozan1@gmail.com>
2025-03-30 03:57:06 +0700

    feat: add mailter service using nodemailer

---
+++ config/mailer.config.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  HOST: process.env.MAIL_HOST || 'smtp.example.com',
+  PORT: process.env.MAIL_PORT || 587,
+  USER: process.env.MAIL_USER || "user",
+  PASSWORD: process.env.MAIL_PASSWORD || "password",
+}
--- services/mailer.service.js
+++ services/mailer.service.js
@@ -1,5 +1,18 @@
-export const sendMail = async (to, subject, recipient) => {
-  // Simulate sending an email
-  console.log(`Sending email to ${to} with subject "${subject}" and recipient "${recipient}"`);
-  return Promise.resolve();
-}
+import nodemailer from 'nodemailer';
+import config from '../config/mailer.config.js';
+
+/**
+ * Mailer service for sending emails
+ * @module services/mailer.service
+ */
+
+const transporter = nodemailer.createTransport({
+  host: config.HOST,
+  port: config.PORT,
+  auth: {
+    user: config.USER,
+    pass: config.PASSWORD,
+  },
+});
+
+export { transporter as mailer };
